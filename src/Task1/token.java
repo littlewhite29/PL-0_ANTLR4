43a86ed27d893f0667991bf90077505831578d15
LW <littlewhite29@qq.com>
2023-12-29 13:57:10 +0800

    导入lombok

---
+++ src/Task1/token.java
@@ -0,0 +1,14 @@
+package Task1;
+
+import lombok.Data;
+
+/**
+ * @author LW
+ * @date 2023/12/29 13:52
+ */
+@Data
+public class token {
+    private tokenEnum tokenEnum;
+
+    private String tokenValue;
+}
---
+++ src/Task1/tokenEnum.java
@@ -0,0 +1,8 @@
+package Task1;
+
+/**
+ * @author LW
+ * @date 2023/12/29 13:53
+ */
+public enum tokenEnum {
+}
